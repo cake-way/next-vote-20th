2ccb806f13316aaf9c2a96f240419807bb7a01ec
지민재 <jim400934@gmail.com>
2025-01-02 11:25:44 +0900

    Fix: hydration mismatch 해결하기

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true, // styled-components 활성화
+  },
 };
 
 export default nextConfig;
